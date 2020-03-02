1cce6b5edf10bee66cc4cdfcfbe3a41d65b28c6e
rergovich <rachelergovich@creighton.edu>
2020-03-02 16:31:08 -0600

    completed mad libs and four controls
    Completed the requirements for the AB forms

---
+++ forms/ABFourControls/ABFourControls.js
@@ -0,0 +1,16 @@
+
+chbxChoice.onclick=function(){
+   NSB.MsgBox(`You made a choice!`)
+}
+
+rbtnChoice.onclick=function(){
+  NSB.MsgBox(`Nice!`)
+}
+
+sldrBlue_input.onchange=function(){
+  NSB.MsgBox(`Congrats, your mouse works!`)
+}
+
+slctChoice.onclick=function(){
+  NSB.MsgBox(`Good choice!`)
+}
